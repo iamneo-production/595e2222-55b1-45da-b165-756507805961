7fd977c99f54d7856041e2a3600858d1fef99dcb
sahulrao <69004407+sahulrao@users.noreply.github.com>
2022-04-07 22:40:29 +0530

    Update Cards.jsx

--- reactapp/src/Cads/Cards.jsx
+++ reactapp/src/Cads/Cards.jsx
@@ -4,6 +4,14 @@ import img1 from '../assets/pic1.jpg';
 import img2 from '../assets/pic2.jpg';
 import img3 from '../assets/pic3.jpg';
 import img4 from '../assets/pic4.jpg';
+import img5 from '../assets/pic5.jpg';
+import img6 from '../assets/pic6.jpg';
+import img7 from '../assets/pic7.jpg';
+import img8 from '../assets/pic8.jpg';
+import img9 from '../assets/pic9.jpg';
+import img10 from '../assets/pic10.jpg';
+import img11 from '../assets/pic11.jpg';
+import img12 from '../assets/pic12.jpg';
 
 
 class Cards extends Component{
@@ -26,30 +34,30 @@ class Cards extends Component{
 
 
                     <div className="col-md-3">
-                        <Card imgsrc={img1} title="justin bieber" songsdigit="0"/>
+                        <Card imgsrc={img5} title="ar rahaman" songsdigit="0"/>
                     </div>
                     <div className="col-md-3">
-                    <Card imgsrc={img2} title="ariana grande" songsdigit="0"/>
+                    <Card imgsrc={img6} title="dua lipa" songsdigit="0"/>
                     </div>
                     <div className="col-md-3">
-                    <Card imgsrc={img3} title="shawn mendes" songsdigit="0"/>
+                    <Card imgsrc={img7} title="post malone" songsdigit="0"/>
                     </div>
                     <div className="col-md-3">
-                    <Card imgsrc={img4} title="bts" songsdigit="0"/>
+                    <Card imgsrc={img8} title="lil nas x" songsdigit="0"/>
                     </div>
 
 
                     <div className="col-md-3">
-                        <Card imgsrc={img1} title="justin bieber" songsdigit="0"/>
+                        <Card imgsrc={img9} title="arijit singh" songsdigit="0"/>
                     </div>
                     <div className="col-md-3">
-                    <Card imgsrc={img2} title="ariana grande" songsdigit="0"/>
+                    <Card imgsrc={img10} title="shreya goshal" songsdigit="0"/>
                     </div>
                     <div className="col-md-3">
-                    <Card imgsrc={img3} title="shawn mendes" songsdigit="0"/>
+                    <Card imgsrc={img11} title="sunandha sharma" songsdigit="0"/>
                     </div>
                     <div className="col-md-3">
-                    <Card imgsrc={img4} title="bts" songsdigit="0"/>
+                    <Card imgsrc={img12} title="jass manak" songsdigit="0"/>
                     </div>
 
 
